37e77897008d7bd7ad081ac4a22327f243de49ed
DaveAyeni <118154926+DaveAyeni@users.noreply.github.com>
2022-12-09 15:37:01 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - entry
  * Return: Always 0
  */
-int main(void)
+ int main(void)
 {
-    puts("\"programming is like building a multilingual puzzle");
+  puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }	
